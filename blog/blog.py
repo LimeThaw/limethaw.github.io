b012bf51b32fee4c6b39601e69447c4c21c0d290
LimeThaw <timolaudi@gmail.com>
2023-01-22 11:53:29 +0100

    Adding blog article creation

---
+++ blog/blog.py
@@ -0,0 +1,57 @@
+from argparse import ArgumentParser
+import datetime
+import json
+import shutil
+from string import Template
+import time
+import uuid
+
+JSON_FILE = "index.json"
+
+def generate_article(title):
+    print("generating article '"+title+"'...")
+
+    filename = Template("$time-$title.md").substitute({
+        "time": datetime.datetime.now().strftime("%y-%m-%d_%H-%M"),
+        "title": title,
+    })
+    shutil.copyfile("template.md", "posts/"+filename)
+
+    my_uuid = uuid.uuid4().hex
+    timestamp = time.time()
+    with open(JSON_FILE, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data[my_uuid] = {
+        "timestamp": timestamp,
+        "filename": filename,
+    }
+
+    with open(JSON_FILE, "w") as jsonFile:
+        json.dump(data, jsonFile, indent=4)
+
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'action',
+        help="What would you like to do?",
+        choices=["new", "generate"],
+    )
+    parser.add_argument(
+        "--title",
+        required=False,
+        default="Title",
+    )
+    args = parser.parse_args()
+
+    if args.action == "new":
+        generate_article(args.title)
+    elif args.action == "generate":
+        regenerate_blog()
+    else:
+        print("Error: "+args)
+
+if __name__ == '__main__':
+    main()
---
+++ blog/posts/23-01-22_11-44-Making a Blogging Tool.md
@@ -0,0 +1,21 @@
+Making a Blogging Tool
+--
+
+I decided to record my projects a little bit better. I hope that this helps
+me reflect a little bit more on what I am doing and why, and learn a bit more
+from my adventures. For this purpose I decided to start this little blog.
+
+First things first, I needed to settle on a platform. Since I only intend
+to share static content here, and I am already using GitHub a lot, I decided
+to use GitHubs pages infrastructure to host the blog. I didn't immediately find
+a blogging software that I really liked, since most seemed to be a little much
+setup for what I need. And since I am a programmer and looking for projects to
+learn new things, I decided to make my own. I settled on simple markdown as
+format to write the articles, JSON to store metainfo in a concise manner that
+is readable to both machines and humans, and python to write a little command
+line tool to help me manage everything.
+
+As of writing this, I am implementing the Python tool. It seems interesting
+to create such a minimalistic piece of software, since I don't really have
+to think too much about use- and edge cases, but just conde in what I need
+right now.
