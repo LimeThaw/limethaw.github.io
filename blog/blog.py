f011fd7cadfb5e8f299f7a062e31ab65c24aa407
LimeThaw <timolaudi@gmail.com>
2023-01-22 14:20:55 +0100

    Markdown generieren

--- blog/blog.py
+++ blog/blog.py
@@ -5,17 +5,21 @@ import shutil
 from string import Template
 import time
 import uuid
+import markdown
 
 JSON_FILE = "index.json"
+PAGE_TEMPLATE = "templates/page.html"
+ARTICLE_TEMPLATE = "templates/article.html"
+ARTICLE_MARKDOWN = "templates/article_markdown.md"
+ARTICLES_PATH = "posts/"
+OUTPUT_FILE = "../index.html"
 
 def generate_article(title):
-    print("generating article '"+title+"'...")
-
     filename = Template("$time-$title.md").substitute({
         "time": datetime.datetime.now().strftime("%y-%m-%d_%H-%M"),
         "title": title,
     })
-    shutil.copyfile("templates/article_markdown.md", "posts/"+filename)
+    shutil.copyfile(ARTICLE_MARKDOWN, ARTICLES_PATH+filename)
 
     my_uuid = uuid.uuid4().hex
     timestamp = time.time()
@@ -23,12 +27,49 @@ def generate_article(title):
         data = json.load(jsonFile)
 
     data[my_uuid] = {
+        "title": title,
         "timestamp": timestamp,
         "filename": filename,
     }
 
     with open(JSON_FILE, "w") as jsonFile:
         json.dump(data, jsonFile, indent=4)
+
+    print("\u2713 Generated article " + title + "; You can add your content in " + ARTICLES_PATH+filename)
+
+
+
+def regenerate_blog():
+    with open(PAGE_TEMPLATE, "r") as file:
+        page_template = Template(file.read())
+
+    with open(ARTICLE_TEMPLATE, "r") as file:
+        article_template = Template(file.read())
+        
+    with open(JSON_FILE, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    articles = ""
+    for key in data:
+        this_data = data[key]
+        this_date = datetime.datetime.utcfromtimestamp(this_data["timestamp"]).strftime('%d.%m.%Y')
+
+        with open(ARTICLES_PATH+this_data["filename"], "r") as file:
+            this_content = markdown.markdown(file.read()) # TODO: Parse Markdown
+
+        articles = article_template.substitute({
+            "title": this_data["title"],
+            "date": this_date,
+            "content": this_content,
+        }) + articles
+
+    page = page_template.substitute({
+        "articles": articles
+    })
+    with open(OUTPUT_FILE, "w") as file:
+        file.write(page)
+
+    print("\u2713 Generated " + OUTPUT_FILE)
 
 
 
--- blog/posts/23-01-22_11-44-Making a Blogging Tool.md
+++ blog/posts/23-01-22_11-44-Making a Blogging Tool.md
@@ -1,6 +1,3 @@
-Making a Blogging Tool
---
-
 I decided to record my projects a little bit better. I hope that this helps
 me reflect a little bit more on what I am doing and why, and learn a bit more
 from my adventures. For this purpose I decided to start this little blog.
@@ -29,3 +26,21 @@ Bootstrap, which is handy, but I don't have a lot of experience with it, so
 I scavenged some online examples and tutorials for inspiration. Keeping in
 line with the tone of this project, the design is very minimalistic and only
 contains what is absolutely necessary for the current state.
+
+For the final part, the generation of the actual website. I split my design
+into one template for the page, and one for an article. I then used Python's
+Template substitution to insert my data in a two-step process, one substitution
+for every article, and then the insertion into the page. Thanks to Python's
+extensive library ecosystem I could simply use a library to translate the
+Markdown into HTML.
+
+So for now, this blog page seems to be complete. It is very simplistic,
+but does exactly what I want for now. Next steps could include a better
+design, dev-ops and monitoring of some kind, or an article view to read
+single articles. These could be generated into their own HTML files,
+which would probably even scale, since the required space is still roughly
+linear to the number of articles. If I want to reduce redundancy, I could
+also load the article part using JavaScript, so I only have to store the
+page part of the template once.
+
+But I'm getting ahead of myself. For now I'm done.
